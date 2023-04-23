7d6f519ff6c1b2723f69ed2a4db800c06b73416b
Vlas Nagibin <nagibinvlas@gmail.com>
2023-04-24 00:02:04 +0300

    Edit test classes

--- src/test/java/com/example/testproject/cache/FirstLevelCacheTest.java
+++ src/test/java/com/example/testproject/cache/FirstLevelCacheTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@SpringBootTest(classes = FirstLevelCacheTest.class)
 class FirstLevelCacheTest {
 
 //    @PersistenceContext
--- src/test/java/com/example/testproject/orphanremoval/OrphanRemovalTest.java
+++ src/test/java/com/example/testproject/orphanremoval/OrphanRemovalTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@SpringBootTest(classes = OrphanRemovalTest.class)
 class OrphanRemovalTest {
 
 //    @Autowired
