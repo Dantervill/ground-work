ec047c1a23fd169f502c6c01e6f66ffddc96188b
Vlas Nagibin <nagibinvlas@gmail.com>
2023-04-24 20:01:45 +0300

    Add FirstLevelCacheTest.java

--- src/test/java/com/example/testproject/cache/FirstLevelCacheTest.java
+++ src/test/java/com/example/testproject/cache/FirstLevelCacheTest.java
@@ -11,19 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(classes = FirstLevelCacheTest.class)
 class FirstLevelCacheTest {
 
-//    @PersistenceContext
-//    private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-//    @Test
-//    @Transactional
-//    void when2Finds_thenOneFind() {
-//        System.out.println("EntityManager/Session is open: " + em.isOpen());
-//        City city = em.find(City.class, 1L);
-//        System.out.println("finding for the 1 time");
-//        Assertions.assertEquals("Moscow", city.getName());
-//        City city2 = em.find(City.class, 1L);
-//        System.out.println("finding for the 2 time");
-//        Assertions.assertEquals("Moscow", city2.getName());
-//        System.out.println("EntityManager/Session is open: " + em.isOpen());
-//    }
+    @Test
+    @Transactional
+    void when2Finds_thenOneFind() {
+        System.out.println("EntityManager/Session is open: " + em.isOpen());
+        City city = em.find(City.class, 1L);
+        System.out.println("finding for the 1 time");
+        Assertions.assertEquals("Moscow", city.getName());
+        City city2 = em.find(City.class, 1L);
+        System.out.println("finding for the 2 time");
+        Assertions.assertEquals("Moscow", city2.getName());
+        System.out.println("EntityManager/Session is open: " + em.isOpen());
+    }
 }
